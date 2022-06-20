c4daf979d08d902cce2ffd675a89a5cc359088ba
Youssef-Mahmoud <joshuman526@gmail.com>
2022-06-20 15:38:10 +0200

    Edit Files

--- dist/js/all.js
+++
@@ -1,1 +0,0 @@
-let toggelIcon=document.querySelector(".toggle-icon"),links=document.querySelector(".hidden-links"),body=document.body;console.log(toggelIcon),toggelIcon.addEventListener("click",e=>{links.style.left="0"});
