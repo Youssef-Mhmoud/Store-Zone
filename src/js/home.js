b2f8e6e3f96d6401c9a865fe27561c0344c1b3be
Youssef-Mahmoud <joshuman526@gmail.com>
2022-06-22 14:57:58 +0200

    Edit

--- src/js/home.js
+++ src/js/home.js
@@ -6,15 +6,15 @@ let preBtn = [document.querySelector(".pre")]
 
 productContainers.forEach((item, i) => {
   let containerDimenstions = item.getBoundingClientRect();
-  let containerWidth = containerDimenstions.width;
+  let containerWidth = containerDimenstions.width - 2000;
 
   // Next Button
   nextBtn[i].addEventListener('click', (e) => {
-    item.scrollLeft += containerWidth
+    item.scrollLeft -= containerWidth
   })
   // Previous Button
   preBtn[i].addEventListener('click', (e) => {
-    item.scrollLeft -= containerWidth
+    item.scrollLeft += containerWidth
   })
 })
 
