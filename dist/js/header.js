69d1c2da963ee6ea94e5ff84f879a33ed576186d
Youssef-Mahmoud <joshuman526@gmail.com>
2022-06-29 00:35:30 +0200

    Responsive Home Page

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,1 +1,1 @@
-let barBtn=document.querySelector("#mobile .menu-hover"),ulList=document.querySelector(".nav-items ul");barBtn.addEventListener("click",e=>{ulList.classList.add("active")});
+let barBtn=document.querySelector("#mobile .menu-hover"),ulList=document.querySelector(".nav-items ul"),xMark=document.getElementById("close");barBtn.addEventListener("click",e=>{ulList.classList.add("active")}),xMark.addEventListener("click",e=>{ulList.classList.remove("active")});
--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,11 @@
 let barBtn = document.querySelector('#mobile .menu-hover')
 let ulList = document.querySelector('.nav-items ul')
-
+let xMark = document.getElementById('close')
 
 barBtn.addEventListener('click', (eo) => {
-    ulList.classList.add('active')
+  ulList.classList.add('active')
+})
 
+xMark.addEventListener('click', (eo) => {
+  ulList.classList.remove('active')
 })
