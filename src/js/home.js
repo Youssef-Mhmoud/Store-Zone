8c4d1c70ac6d4a26577112cd52573ae46dc17af7
Youssef-Mahmoud <joshuman526@gmail.com>
2022-06-28 22:40:06 +0200

    Add Banner Seciton In Home Page

--- src/js/home.js
+++ src/js/home.js
@@ -1,20 +1,18 @@
-/* Slider */
 // Get Elements
-let productContainers = [...document.querySelectorAll(".product-container")]
-let nextBtn = [...document.querySelectorAll(".next")]
-let preBtn = [...document.querySelectorAll(".pre")]
-
-productContainers.forEach((item, i) => {
-  let containerDimenstions = item.getBoundingClientRect();
-  let containerWidth = containerDimenstions.width - 400;
-
-  // Next Button
-  nextBtn[i].addEventListener('click', (e) => {
-    item.scrollLeft += containerWidth
-  })
-  // Previous Button
-  preBtn[i].addEventListener('click', (e) => {
-    item.scrollLeft -= containerWidth
-  })
-})
+  let productContainers = [...document.querySelectorAll(".product-container")];
+  let nextBtn = [...document.querySelectorAll(".next")];
+  let preBtn = [...document.querySelectorAll(".pre")];
+  productContainers.forEach((item, i) => {
+    let containerDimenstions = item.getBoundingClientRect();
+    let containerWidth = containerDimenstions.width - 400;
+    
+    // Next Button
+    nextBtn[i].addEventListener("click", (e) => {
+      item.scrollLeft += containerWidth;
+    });
+    // Previous Button
+    preBtn[i].addEventListener("click", (e) => {
+      item.scrollLeft -= containerWidth;
+    });
+  });
 
