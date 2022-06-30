37b34ed1ea841000b1cc6fc92a78aa7a08acb194
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-06-30 04:55:09 +0200

    Delete Search File

--- src/js/product.js
+++ src/js/product.js
@@ -9,35 +9,6 @@ sizeBtns.forEach((item) => {
   });
 });
 
-// Make Products Dynamik
-const setData = (data) => {
-  let title = document.querySelector("title")
-  title.innerHTML += data.data[0].name
-  // Set Image 
-  let img = document.querySelector('.img-slider')
-  img.style.backgroundImage = `url(img/${data.data[0].images})`
-  // Set Text 
-  let name = document.querySelector('.details h2')
-  let shortDes = document.querySelector('.details p')
-  
-  name.innerHTML += data.data[0].name
-  shortDes.innerHTML += data.data[0].des
-  // Set Price 
-  let price = document.querySelector('.price')
-  let actPrice = document.querySelector('.actual-price')
-  let actPriceDis = document.querySelector('.actual-price-dis')
 
-  price.innerHTML += `${data.data[0].actualPrice}$`
-  actPrice.innerHTML += `${data.data[0].sellPrice}$`
-  actPriceDis.innerHTML += `(${data.data[0].discount}% OFF)`
-}
-
-
-async function get() {
-  const res = await fetch("/get-product.json")
-  const data = await res.json()
-  setData(data)
-}
-get()
 
 
